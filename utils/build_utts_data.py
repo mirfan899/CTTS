b9f499a8c51f9e49e782de1eade0839b6c4a35cb
Muhammad Irfan <virtuoso.irfan@gmail.com>
2019-07-29 12:13:19 +0500

    utts data script

---
+++ utils/build_utts_data.py
@@ -0,0 +1,17 @@
+import glob
+import os
+import re
+
+utts = open("../data/cantonese_demo/cantonese_utts.txt", "w", encoding='utf-8')
+for file in glob.glob("../data/txt/*.txt"):
+    lines = open(file, encoding='utf-8').read().strip()
+
+    base_name = os.path.basename(file)
+    name = os.path.splitext(base_name)[0]
+
+    # sentence = "%s %s" % (name.strip(), str(lines.strip()))
+
+    sentence = "{} {}".format(name.strip(), lines.strip())
+    # Some weird space in chinese
+    sentence = re.sub(r"﻿", "", sentence)
+    utts.write(sentence + "\n")

49bc649b3954ce4abd25fbfaa7ff11663d7240f0
Muhammad Irfan <virtuoso.irfan@gmail.com>
2019-08-09 10:36:44 +0500

    path fixed.

--- tools/run_mfa.sh
+++ tools/run_mfa.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # get current working directory
 tools_dir=$(dirname $0)
 cd ${tools_dir}
-echo ${tools_dir}
 
 mfa_path="./montreal-forced-aligner"
 data_mfa_url=https://github.com/MontrealCorpusTools/Montreal-Forced-Aligner/releases/download/v1.0.0/montreal-forced-aligner_linux.tar.gz
@@ -13,4 +12,4 @@ if [[ ! -d ${mfa_path} ]]; then
 fi
 
 cd ${mfa_path}
-bin/mfa_train_and_align /MTTS/data/cantonese_demo/wav /MTTS/misc/cantonese_mtts.lexicon  /MTTS/data/cantonese_demo/output/textgrid
+bin/mfa_train_and_align  ../../data/cantonese_demo/wav  ../../misc/cantonese_mtts.lexicon   ../../data/cantonese_demo/output/textgrid
